7cace36037f25e392aee818959993fa653c892f4
Athyuttam Eleti <athyuttamre@gmail.com>
2016-12-31 20:06:33 +0530

    Add stencil code

---
+++ client/client.go
@@ -0,0 +1,18 @@
+package client
+
+import (
+	"../whatsup"
+
+	"fmt"
+)
+
+func Start(user string, serverPort string, serverAddr string) {
+	// Connect to chat server
+	_, err := whatsup.ServerConnect(user, serverAddr, serverPort)
+	if err != nil {
+		fmt.Printf("unable to connect to server: %s\n", err)
+		return
+	}
+	// TODO: Receive input from the user and use the first return value of whatsup.ServerConnect
+	// (currently ignored so the stencil will compile) to talk to the server.
+}
---
+++ server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"../whatsup"
+
+	"fmt"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	// TODO: Implement handling messages from a client.
+	// You will find whatsup.SendMsg and whatsup.RecvMsg methods useful for
+	// serializing and deserializing messages.
+
+}
+
+func Start() {
+	listen, port, err := whatsup.OpenListener()
+	fmt.Printf("Listening on port %d\n", port)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		conn, err := listen.Accept() // this blocks until connection or error
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
